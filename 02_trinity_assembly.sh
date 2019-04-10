7fa4b404d95b2929a9de195a8356c02d53a8bd39
Jack Hruska <jph239@cornell.edu>
2019-04-10 16:59:34 -0500

    Create 02_trinity_assembly.sh

---
+++ 02_trinity_assembly.sh
@@ -0,0 +1,10 @@
+##introductory trinity assembly run, using Trinity version 2.1.1###
+
+
+Trinity --seqType fa \
+    --left ./01_filtered_data/B_bicolor_G_LHD1272_R1.fastq.gz,./01_filtered_data/B_bicolor_G_LHD1279_R1.fastq.gz,./01_filtered_data/B_bicolor_G_LHD1280_R1.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1272_R1.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1279_R1.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1280_R1.fastq.gz \
+    --right ./01_filtered_data/B_bicolor_G_LHD1272_R2.fastq.gz,./01_filtered_data/B_bicolor_G_LHD1279_R2.fastq.gz,./01_filtered_data/B_bicolor_G_LHD1280_R2.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1272_R2.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1279_R2.fastq.gz,./01_filtered_data/B_bicolor_L_LHD1280_R2.fastq.gz \
+    --max-memory 120G \
+    --CPU 6 \
+    --output ./trinity_out
+    
