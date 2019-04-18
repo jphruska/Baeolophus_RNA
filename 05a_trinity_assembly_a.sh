1a8fcd977629417a52c6cd43e1e92536b1bb19c3
Jack Hruska <jph239@cornell.edu>
2019-04-18 11:12:10 -0500

    Update 05a_trinity_assembly_a.sh

--- 05a_trinity_assembly_a.sh
+++ 05a_trinity_assembly_a.sh
@@ -1,1 +1,10 @@
-o
+### includes RF flag ###
+
+
+Trinity --seqType fa \
+    --left ../01_filtered_data/B_bicolor_L_LHD1272_R1.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1279_R1.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1280_R1.fastq.gz \
+    --right ../01_filtered_data/B_bicolor_L_LHD1272_R2.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1279_R2.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1280_R2.fastq.gz \
+    --max-memory 32G \
+    --CPU 6 \
+    --SS_lib_type RF \
+    --output ../trinity_liver_output_a
