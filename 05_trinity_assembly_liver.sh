13803fbd80c4701c12e9a5b4b547c26df536e26a
Jack Hruska <jph239@cornell.edu>
2019-04-18 11:06:55 -0500

    Update 05_trinity_assembly_liver.sh

--- 05_trinity_assembly_liver.sh
+++ 05_trinity_assembly_liver.sh
@@ -1,1 +1,7 @@
 
+Trinity --seqType fa \
+    --left ../01_filtered_data/B_bicolor_L_LHD1272_R1.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1279_R1.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1280_R1.fastq.gz \
+    --right ../01_filtered_data/B_bicolor_L_LHD1272_R2.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1279_R2.fastq.gz,../01_filtered_data/B_bicolor_L_LHD1280_R2.fastq.gz \
+    --max-memory 120G \
+    --CPU 6 \
+    --output ../trinity_liver_output
